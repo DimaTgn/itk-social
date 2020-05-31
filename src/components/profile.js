b02630f445c18d3c6b389e215b41e5bf83c583fc
DimaTgn <dmitriy_demyanenko@mail.ru>
2020-05-31 15:26:20 +0300

    components

---
+++ src/components/profile.js
@@ -0,0 +1,26 @@
+import React, { Component} from 'react';
+
+
+const Profile = () =>{
+    return <div className='app-content'>
+        <div>
+            <img src="https://dcea.ru/wp-content/uploads/2019/10/fejlec.jpg"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
